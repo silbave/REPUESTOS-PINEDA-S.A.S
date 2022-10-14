e272d62dd1dfd9953f104e2b923d4ef9f15fd9b4
silbave <114703318+silbave@users.noreply.github.com>
2022-10-13 23:23:17 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php
+$Nombre= $_POST{'Nombre'};
+$Número telefónico= $_POST{'Número telefónico'};
+$Dirección de correo= $_POST{'Dirección de correo'};
+$Tema= $_POST{'Tema'};
+$Mensaje= $_POST{'Mensaje'};
+
+echo "<h2>informacion recevida en php</h2>";
+echo "el nombre recivido es:" . $Nombre . "<br/>";
+echo "el nombre recivido es:" . $Número telefónico . "<br/>";
+echo "el nombre recivido es:" . $Dirección de correo . "<br/>";
+echo "el nombre recivido es:" . $Tema . "<br/>";
+echo "el nombre recivido es:" . $Mensaje . "<br/>";
